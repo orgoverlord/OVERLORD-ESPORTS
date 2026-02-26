f78e5cc0acae85a559c1c1bc024fafa367a1014d
orgoverlord <orgoverlord@gmail.com>
2026-02-26 16:46:33 +0530

    Refactor countdown function for clarity and efficiency

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-function countdown(elementId, targetDate) {
-    var countDownDate = new Date(targetDate).getTime();
+function countdown(id, date) {
+    var end = new Date(date).getTime();
 
-    var x = setInterval(function() {
-
+    setInterval(function(){
         var now = new Date().getTime();
-        var distance = countDownDate - now;
+        var distance = end - now;
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var d = Math.floor(distance / (1000*60*60*24));
+        var h = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+        var m = Math.floor((distance % (1000*60*60))/(1000*60));
+        var s = Math.floor((distance % (1000*60))/1000);
 
-        document.getElementById(elementId).innerHTML =
-        days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+        document.getElementById(id).innerHTML =
+        d+"d "+h+"h "+m+"m "+s+"s";
 
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById(elementId).innerHTML = "EXPIRED";
-        }
-    }, 1000);
+    },1000);
 }
 
-window.onload = function() {
-    countdown("regCountdown", "Feb 28, 2026 18:00:00");
-    countdown("startCountdown", "Mar 1, 2026 11:00:00");
+window.onload=function(){
+    countdown("regCountdown","Feb 28, 2026 18:00:00");
+    countdown("startCountdown","Mar 1, 2026 11:00:00");
 }
